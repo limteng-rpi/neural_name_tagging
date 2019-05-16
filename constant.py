dfbdbb727c612791721f0e3ab2dfcc7b1f0e43f7
limteng-rpi <linying@limteng.com>
2019-05-16 10:28:29 -0400

    Initial commit

---
+++ constant.py
@@ -0,0 +1,31 @@
+PAD = '<$PAD$>'
+UNK = '<$UNK$>'
+EOS = '<$EOS$>'
+SOS = '<$SOS$>'
+PAD_INDEX = 0       # Padding symbol index
+UNK_INDEX = 1       # Unknown token index
+SOS_INDEX = 2       # Start-of-sentence symbol index
+EOS_INDEX = 3       # End-of-sentence symbol index
+CHAR_PADS = [
+    (PAD, PAD_INDEX),
+    (UNK, UNK_INDEX),
+]
+TOKEN_PADS = [
+    (PAD, PAD_INDEX),
+    (UNK, UNK_INDEX),
+    (SOS, SOS_INDEX),
+    (EOS, EOS_INDEX),
+]
+
+TOKEB_REPLACE = {
+    '-LRB-': '(',
+    '-RRB-': ')',
+    '-LSB-': '[',
+    '-RSB-': ']',
+    '-LCB-': '{',
+    '-RCB-': '}',
+    '``': '"',
+    '\'\'': '"',
+    '/.': '.',
+    '/?': '?'
+}
