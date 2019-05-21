0ca6326933cd6089cbf2ddb5f7532ee6da1108fc
limteng-rpi <linying@limteng.com>
2019-05-20 20:59:16 -0400

    train lstmcnns

--- runs/train_lstmcnn.py
+++ runs/train_lstmcnn.py
@@ -14,8 +14,9 @@ from torch.utils.data import DataLoader
 
 from data import ConllParser, NameTaggingDataset
 from model import LstmCNN
-from util import counter_to_vocab, merge_vocabs, build_embedding_vocab,\
-    build_form_mapping, build_signal_embed, load_vocab
+from util import counter_to_vocab, merge_vocabs, build_embedding_vocab, \
+    build_form_mapping, build_signal_embed, load_vocab, \
+    calculate_labeling_scores, save_result_file, calculate_lr
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s-%(levelname)s: %(message)s')
@@ -24,9 +25,6 @@ logger = logging.getLogger()
 # parse commandline arguments
 parser = ArgumentParser()
 # i/o
-# parser.add_argument('--train', help='path to the train file')
-# parser.add_argument('--dev', help='path to the dev file')
-# parser.add_argument('--test', help='path to the test file')
 parser.add_argument('-i', '--input', help='path to the input directory')
 parser.add_argument('-o', '--output', help='path to the output directory')
 # training parameters
@@ -34,6 +32,7 @@ parser.add_argument('--lr', type=float, default=1e-3, help='learning rate')
 parser.add_argument('-b', '--batch_size', type=int, default=10)
 parser.add_argument('-m', '--max_epoch', type=int, default=20)
 parser.add_argument('-s', '--seed', type=int, default=1111)
+parser.add_argument('--eval_step', type=int, default=-1)
 # model parameters
 parser.add_argument('-e', '--embed')
 parser.add_argument('--embed_vocab', default=None)
@@ -44,7 +43,6 @@ parser.add_argument('--char_feat_dim', type=int, default=100)
 parser.add_argument('--lstm_size', type=int, default=100)
 parser.add_argument('--lstm_dropout', type=float, default=.5)
 parser.add_argument('--feat_dropout', type=float, default=.5)
-# parser.add_argument('--signal_dropout', type=float, default=.2)
 parser.add_argument('--char_type', default='ffn',
                     help='ffn: feed-forward netword; hw: highway network')
 # gpu
@@ -60,7 +58,6 @@ timestamp = time.strftime('%Y%m%d_%H%M%S', time.localtime())
 output_dir = os.path.join(args.output, timestamp)
 os.mkdir(output_dir)
 best_model_file = os.path.join(output_dir, 'model.best.mdl')
-# last_model_file = os.path.join(output_dir, 'model.last.mdl')
 dev_result_file = os.path.join(output_dir, 'result.dev.bio')
 test_result_file = os.path.join(output_dir, 'result.test.bio')
 logger.info('Output directory: {}'.format(output_dir))
@@ -95,6 +92,7 @@ else:
 token_vocab = load_vocab(os.path.join(args.input, 'token.vocab.tsv'))
 char_vocab = load_vocab(os.path.join(args.input, 'char.vocab.tsv'))
 label_vocab = load_vocab(os.path.join(args.input, 'label.vocab.tsv'))
+label_itos = {i: l for l, i in label_vocab.items()}
 train_token_counter = train_set.token_counter
 vocabs = dict(token=token_vocab,
               char=char_vocab,
@@ -109,6 +107,9 @@ dev_set.numberize(vocabs)
 test_set.numberize(vocabs)
 
 # create model
+batch_step = len(train_set) // args.batch_size
+total_step = batch_step * args.max_epoch
+eval_step = batch_step if args.eval_step == -1 else args.eval_step
 char_filters = json.loads(args.char_filters)
 model = LstmCNN(vocabs=vocabs,
                 word_embed_file=args.embed_file,
@@ -135,13 +136,74 @@ state = dict(model=model.state_dict(),
              counters=counters)
 
 # training
+global_step = 0
 for epoch in range(args.max_epoch):
     logger.info('Epoch: {}'.format(epoch))
     epoch_loss = []
-
-    for batch_idx, batch in enumerate(DataLoader(train_set,
-                                                 batch_size=args.batch_size,
-                                                 shuffle=True,
-                                                 collate_fn=train_set.batch_processor)):
+    progress = tqdm.tqdm(desc='Epoch: {}'.format(epoch), mininterval=1,
+                         total=batch_step)
+    for batch in DataLoader(train_set,
+                            batch_size=args.batch_size,
+                            shuffle=True,
+                            collate_fn=train_set.batch_processor):
+        global_step += 1
+        progress.update()
         optimizer.zero_grad()
-        pass
+        token_ids, char_ids, label_ids, seq_lens, _, _ = batch
+        loglik, _ = model.forward(token_ids, char_ids, seq_lens, label_ids)
+        loss = -loglik.mean()
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 5.0)
+        optimizer.step()
+        epoch_loss.append(loss.item())
+
+        # evaluate the model
+        if global_step % eval_step == 0 or global_step == total_step:
+            # dev set
+            best_epoch = False
+            results = []
+            for batch_dev in DataLoader(dev_set, batch_size=50, shuffle=False,
+                                        collate_fn=dev_set.batch_processor):
+                (token_ids, char_ids, label_ids, seq_lens,
+                 tokens, labels) = batch_dev
+                preds = model.predict(token_ids, char_ids, seq_lens)
+                preds = [[label_itos[l] for l in ls] for ls in preds]
+                results.append((preds, labels, tokens, seq_lens))
+            fscore, prec, rec = calculate_labeling_scores(results)
+            print()
+            logger.info('Dev - P: {:.2f} R: {:.2f} F: {:.2f}'.format(
+                fscore, prec, rec))
+            if fscore > best_scores['dev']['f']:
+                best_epoch = True
+                best_scores['dev'] = {'f': fscore, 'p': prec, 'r': rec}
+                torch.save(state, best_model_file)
+                save_result_file(results, dev_result_file)
+
+            # test set
+            results = []
+            for batch_test in DataLoader(test_set, batch_size=50, shuffle=False,
+                                         collate_fn=test_set.batch_processor):
+                (token_ids, char_ids, label_ids, seq_lens,
+                 tokens, labels) = batch_test
+                preds = model.predict(token_ids, char_ids, seq_lens)
+                preds = [[label_itos[l] for l in ls] for ls in preds]
+                results.append((preds, labels, tokens, seq_lens))
+            fscore, prec, rec = calculate_labeling_scores(results)
+            logger.info('Test - P: {:.2f} R: {:.2f} F: {:.2f}'.format(
+                fscore, prec, rec))
+            print()
+            if best_epoch:
+                best_scores['test'] = {'f': fscore, 'p': prec, 'r': rec}
+                save_result_file(results, test_result_file)
+
+            lr = calculate_lr(args.lr, global_step, total_step,
+                              min_lr=0.01 * args.lr)
+            for p in optimizer.param_groups:
+                p['lr'] = lr
+
+    logger.info('Best dev: P: {:.2f}, R: {:.2f}, F: {:.2f}'.format(
+        best_scores['dev']['p'], best_scores['dev']['r'],
+        best_scores['dev']['f']))
+    logger.info('Best test: P: {:.2f}, R: {:.2f}, F: {:.2f}'.format(
+        best_scores['test']['p'], best_scores['test']['r'],
+        best_scores['test']['f']))
